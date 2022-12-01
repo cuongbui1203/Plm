7164e8973c2352832b4e5477c34b4f8033d1ec17
cuongbui <cuongbuimanh1203@gmail.com>
2022-12-01 08:39:46 +0700

    add

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,41 +2,21 @@ import './App.css';
 import HeaderBar from './components/HeaderBar';
 import SideBar from './components/SideBar';
 import Product from './components/Product';
-<<<<<<< HEAD
 import RegisterForm from './components/registerForm/registerForm';
 import LoginForm from './components/loginForm/LoginForm';
 function App() {
   return (
     <div>
       {/* <HeaderBar />
-=======
-import Login from './login/Login';
-
-function App() {
-  return (
-    <div>
-      {/* <Login /> */}
-      <div>
->>>>>>> 6ed33ff0d1aa8d3217439886da8814e1401c85c6
 
       <HeaderBar />
       </div>
       <div>
       <SideBar />
 
-<<<<<<< HEAD
       <Product /> */}
       {/* <RegisterForm /> */}
       <LoginForm />
-=======
-      </div>
-      <div>
-      <Product />
-
-      </div>
-
-
->>>>>>> 6ed33ff0d1aa8d3217439886da8814e1401c85c6
     </div>
   );
 }
